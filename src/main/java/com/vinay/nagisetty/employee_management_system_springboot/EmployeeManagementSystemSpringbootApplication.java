47d99d29f4876000baa7accd42edffe74e34c21d
vinay <vinaynagisetty7@gmail.com>
2024-12-01 20:16:17 +0530

    created spring project and integrated with mysql db

---
+++ src/main/java/com/vinay/nagisetty/employee_management_system_springboot/EmployeeManagementSystemSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.vinay.nagisetty.employee_management_system_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSystemSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSystemSpringbootApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/vinay/nagisetty/employee_management_system_springboot/EmployeeManagementSystemSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinay.nagisetty.employee_management_system_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
